c7545fdbadd34e689043bebe86cbf4316acbf4f7
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-04-04 22:14:57 +0300

    Final Version by IvanAndGero

--- src/sample/Start.java
+++ src/sample/Start.java
@@ -2,16 +2,16 @@ package sample;
 
 import javafx.application.Application;
 
-//public class Start {
-//   public static void main(String[] args) {
-//       Application.launch(Main.class, args);
-//   }
-//}
-//package sample;
-//     import javafx.application.Application;
-
 public class Start {
+   public static void main(String[] args) {
+       Application.launch(Main.class, args);
+   }
+}/*
+package sample;
+     import javafx.application.Application;*/
+
+/*public class Start {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
-}
+}*/
