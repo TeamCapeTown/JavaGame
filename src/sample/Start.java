096a4f7256b0449eee1053deaf09bcee4f13d4df
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-04-04 22:16:16 +0300

    test ==> Work

--- src/sample/Start.java
+++ src/sample/Start.java
@@ -6,12 +6,4 @@ public class Start {
    public static void main(String[] args) {
        Application.launch(Main.class, args);
    }
-}/*
-package sample;
-     import javafx.application.Application;*/
-
-/*public class Start {
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
-    }
-}*/
+}
