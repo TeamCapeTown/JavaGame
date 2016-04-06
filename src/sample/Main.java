e8aa134e2ed58ba035c0c7c6c90eddd58a00b340
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-04-06 18:22:48 +0300

    Simple changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,17 +9,6 @@ public class Main extends Application {
     public static final String PATH_HIGHSCORE = "res/sample/IO/highScore.txt";
     public static final String PATH_QUESTIONS = "res/sample/IO/questions.txt";
 
-    public static final int NUM_ROUNDS = 5;
-
-    public static final int NUM_QUESTIONS_PER_ROUND = 3;
-
-    public static final int NUM_QUESTION_POSSIBILITIES = 4;
-
-    public static final int RIGHT_ANSWER_SCORE = 100;
-
-    public static final int WRONG_ANSWER_SCORE = -50;
-
-    public static final int MAX_HIGHSCORE_SLOTS = 10;
 
 
     @Override
